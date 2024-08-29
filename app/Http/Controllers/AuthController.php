5ab8185d999f2c4ac3d68fb73d5896fde9b18a77
serpak13 <susafonov@yandex.ru>
2024-08-30 02:23:39 +0500

    add api , controller, requests

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginUSerRequest;
+use App\Http\Requests\StoreUserRequest;
+
+
+class AuthController extends Controller
+{
+    public function register(StoreUserRequest $request){
+
+    }
+
+    public function login(LoginUserREquest $request){
+
+    }
+
+    public function logout(){
+
+    }
+}
